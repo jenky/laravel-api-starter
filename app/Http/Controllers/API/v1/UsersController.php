7c46af419e486bb4b1d902e7755725a7739f0700
Lynh <milano1411@gmail.com>
2015-12-21 10:07:04 +0700

    Remove un-used functions

--- app/Http/Controllers/API/v1/UsersController.php
+++ app/Http/Controllers/API/v1/UsersController.php
@@ -28,16 +28,6 @@ class UsersController extends ApiController
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\API\v1\UserRequest $request
@@ -61,17 +51,6 @@ class UsersController extends ApiController
     public function show(User $user, $id)
     {
         return $this->findResource($user, $id);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
