9f316fcfaa3135377731fd25200eca337e9919cd
Lynh <milano1411@gmail.com>
2016-01-16 22:04:01 +0700

    Publish important configs

---
+++ config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*') 
+    | to accept any value, the allowed methods however have to be explicitly listed.
+    |
+    */
+    
+    'supportsCredentials' => false,
+    'allowedOrigins'      => ['*'],
+    'allowedHeaders'      => ['Accept', 'Authorization', 'Content-Type', 'Origin', 'X-Request-With'],
+    'allowedMethods'      => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    'exposedHeaders'      => [],
+    'maxAge'              => 0,
+    'hosts'               => [],
+    
+];
