c9b7bf94560156b971857bfe3337777f5f0ab7d5
Lynh <milano1411@gmail.com>
2016-02-18 09:45:19 +0700

    Update rate limit

--- app/Http/Middleware/RateLimit.php
+++ app/Http/Middleware/RateLimit.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use Closure;
 use Dingo\Api\Http\InternalRequest;
 use Illuminate\Routing\Middleware\ThrottleRequests;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class RateLimit extends ThrottleRequests
 {
@@ -17,20 +16,13 @@ class RateLimit extends ThrottleRequests
     public function handle($request, Closure $next, $maxAttempts = 60, $decayMinutes = 1)
     {
         if ($request instanceof InternalRequest) {
-            return $next($request)->withHeaders([
-                'X-RateLimit-Limit' => $maxAttempts,
-                'X-RateLimit-Remaining' => $maxAttempts - $this->limiter->attempts($key) + 1,
-            ]);
-        }
+            $key = $this->resolveRequestSignature($request);
+            $response = $next($request);
 
-        $key = $this->resolveRequestSignature($request);
-
-        if ($this->limiter->tooManyAttempts($key, $maxAttempts, $decayMinutes)) {
-            throw new HttpException(429, 'Too Many Attempts.', null, [
-                'Retry-After' => $this->limiter->availableIn($key),
-                'X-RateLimit-Limit' => $maxAttempts,
-                'X-RateLimit-Remaining' => 0,
-            ]);
+            return $this->addHeaders(
+                $response, $maxAttempts,
+                $this->calculateRemainingAttempts($key, $maxAttempts)
+            );
         }
 
         return parent::handle($request, $next, $maxAttempts, $decayMinutes);
