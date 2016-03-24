e273f6573ed4d498d3c2b6f6ef5acc92e7319659
Lynh <milano1411@gmail.com>
2016-03-24 12:48:14 +0700

    New line

--- app/Http/api_routes.php
+++ app/Http/api_routes.php
@@ -10,9 +10,13 @@
 */
 
 $api->version('v1', ['middleware' => $middleware], function ($api) use ($namespace) {
+
     $api->group(['namespace' => $namespace.'v1'], function ($api) {
+
         $api->resources([
             'users' => 'UsersController',
         ]);
+
     });
+
 });
