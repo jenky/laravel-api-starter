9da7666cb8737f93dc6d2e6b1c3a463dbe17fd66
Lynh <jenky.w0w@gmail.com>
2016-06-03 21:51:11 -0400

    Applied fixes from StyleCI

--- app/Http/api_routes.php
+++ app/Http/api_routes.php
@@ -10,13 +10,9 @@
 */
 
 $api->version('v1', ['middleware' => $middleware], function ($api) use ($namespace) {
-
     $api->group(['namespace' => $namespace.'v1'], function ($api) {
-
         $api->resources([
             'users' => 'UsersController',
         ]);
-
     });
-
 });
